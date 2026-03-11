d81af600794b4f2940219acd43731326e34d89d0
Waxxer3 <waxxer3@gmail.com>
2026-03-11 22:03:53 +0300

    4

--- backend/cats/tests.py
+++ backend/cats/tests.py
@@ -8,7 +8,7 @@ from rest_framework.test import APIClient
 class CatsAPITestCase(TestCase):
     def setUp(self):
         user = get_user_model()
-        self.user = User.objects.create_user(username='auth_user')
+        self.user = user.objects.create_user(username='auth_user')
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
 
